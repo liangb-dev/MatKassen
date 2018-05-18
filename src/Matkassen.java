b087f341b90d6de68341431b7cf124bddad7aea8
liangb.dev <liangb.dev@gmail.com>
2018-05-18 15:52:45 +0200

    testing branch changes

--- src/Matkassen.java
+++ src/Matkassen.java
@@ -5,17 +5,6 @@ public class Matkassen{
         Day three = new Day(11,3);
         Day four = new Day(11,4);
 
-        System.out.println("The name of the dishes for this week are: ");
-        System.out.println(one.getTitle());
-        System.out.println(two.getTitle());
-        System.out.println(three.getTitle());
-        System.out.println(four.getTitle());
-
-        System.out.println("The ingredients that you need to buy are: ");
-        one.printIngredients();
-        two.printIngredients();
-        three.printIngredients();
-        four.printIngredients();
 
 
 
